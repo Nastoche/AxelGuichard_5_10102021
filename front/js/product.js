71a59eee6a8c605f5222aca75dc95df878dda09f
Nastoche <85789639+Nastoche@users.noreply.github.com>
2021-10-12 16:56:05 +0200

    product img price and desc

---
+++ front/js/product.js
@@ -0,0 +1,36 @@
+function sofaId() {
+  const urlClient = window.location.href;
+  return urlClient.substring(urlClient.lastIndexOf("=") + 1);
+}
+
+console.log(sofaId());
+
+const apiCall = async function api() {
+  try {
+    const url = `http://localhost:3000/api/products/${sofaId()}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    //   Variables créées pour créer des éléments HTML dynamiques
+
+    const img = document.createElement("img");
+    const itemImg = document.getElementsByClassName("item__img");
+
+    //   Récupérations des données de l'API
+
+    img.src = data.imageUrl;
+    img.alt = data.altTxt;
+
+    // -- Destination des éléments
+
+    itemImg[0].appendChild(img);
+    document.getElementById("title").innerText = data.name;
+    document.getElementById("price").innerText = data.price + " ";
+    document.getElementById("description").innerText = data.description;
+  } catch (err) {
+    console.log(`Erreur : ` + err);
+    return false;
+  }
+};
+
+apiCall();
